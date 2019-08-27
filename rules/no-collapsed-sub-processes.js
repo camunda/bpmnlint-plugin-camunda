543a40eaf5f73ba06779ea9536fdc49ab0d6225a
Nico Rehwaldt <git_nikku@nixis.de>
2019-08-27 11:54:49 +0200

    feat(rules): add no-collapsed-sub-processes rule

---
+++ rules/no-collapsed-sub-processes.js
@@ -0,0 +1,25 @@
+const {
+  is
+} = require('bpmnlint-utils');
+
+
+/**
+ * Rule that reports the usage of collapsed sub-processes.
+ */
+module.exports = function() {
+
+  function check(node, reporter) {
+    if (is(node, 'bpmndi:BPMNShape')) {
+
+      const bpmnElement = node.bpmnElement;
+
+      if (is(bpmnElement, 'bpmn:SubProcess') && node.isExpanded === false) {
+        reporter.report(bpmnElement.id, 'Sub-process should be expanded');
+      }
+    }
+  }
+
+  return {
+    check: check
+  };
+};
