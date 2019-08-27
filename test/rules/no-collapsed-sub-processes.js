7f259bcee22a3c11ec64d49839a93305787854c2
Nico Rehwaldt <git_nikku@nixis.de>
2019-08-27 11:54:49 +0200

    feat(rules): add no-collapsed-sub-processes rule

---
+++ test/rules/no-collapsed-sub-processes.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect;
+
+var readModdle = require('bpmnlint/lib/testers/helper').readModdle;
+
+var RuleTester = require('bpmnlint/lib/testers/rule-tester');
+
+var noCollapsedSubProcessesRule = require('../../rules/no-collapsed-sub-processes');
+
+
+RuleTester.verify('no-collapsed-sub-processes', noCollapsedSubProcessesRule, {
+  valid: [
+    {
+      moddleElement: readModdle(__dirname + '/no-collapsed-sub-processes/valid.bpmn')
+    }
+  ],
+  invalid: [
+    {
+      moddleElement: readModdle(__dirname + '/no-collapsed-sub-processes/invalid.bpmn'),
+      report: {
+        id: 'SubProcess',
+        message: 'Sub-process should be expanded'
+      }
+    }
+  ]
+});
