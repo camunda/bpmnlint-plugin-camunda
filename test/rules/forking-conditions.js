5a27ec6015263d88dce7e267a81507f565fa6bb3
Nico Rehwaldt <git_nikku@nixis.de>
2019-09-13 11:56:05 +0200

    feat(rules): add forking-conditions rule
    Closes #5

---
+++ test/rules/forking-conditions.js
@@ -0,0 +1,35 @@
+var readModdle = require('bpmnlint/lib/testers/helper').readModdle;
+
+var RuleTester = require('bpmnlint/lib/testers/rule-tester');
+
+var forkingConditionsRule = require('../../rules/forking-conditions');
+
+
+RuleTester.verify('forking-conditions', forkingConditionsRule, {
+  valid: [
+    {
+      moddleElement: readModdle(__dirname + '/forking-conditions/valid-conditions.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/forking-conditions/valid-default-flow.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/forking-conditions/valid-join.bpmn')
+    }
+  ],
+  invalid: [
+    {
+      moddleElement: readModdle(__dirname + '/forking-conditions/invalid.bpmn'),
+      report: [
+        {
+          id: 'Flow_2',
+          message: 'Sequence flow is missing condition'
+        },
+        {
+          id: 'Flow_1',
+          message: 'Sequence flow is missing condition'
+        }
+      ]
+    },
+  ]
+});
